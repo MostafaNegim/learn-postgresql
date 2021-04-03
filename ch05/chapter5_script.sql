f3803a95e325e28deaebbf12b2490e440e5afa20
Mostafa Negim <mostafa.negim@gmail.com>
2021-04-03 22:37:57 +0430

    update: update chapter5_script.sql

---
+++ ch05/chapter5_script.sql
@@ -0,0 +1,10 @@
+select * from categories where pk > 12 order by title;
+
+select * from categories where title like 'a%';
+
+select * from categories where title like '%e';
+
+select * from categories where title like '%ap%';
+
+select * from categories where title like 'A%';
+
